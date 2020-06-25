99d5d4c4a33c8c421b4dafa9b7242dabb5b74bb5
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-06-25 12:18:27 +0530

    Initial Commit

---
+++ SegmentedControlUpdates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentedControlUpdates
+//
+//  Created by Prathamesh Kowarkar on 25/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
